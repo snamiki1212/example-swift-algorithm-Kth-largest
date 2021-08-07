e5094424984a4700d284f2dd7fdeda409a874d3b
snamiki1212 <snamiki1212@gmail.com>
2021-08-06 17:32:54 -0700

    Add heap and solution

--- heap/Heap.swift
+++ heap/Heap.swift
@@ -9,15 +9,15 @@ import Foundation
 
 class Solution {
     static func findKthLargest(_ nums: [Int], _ k: Int) -> Int? {
-        let heap = Heap();
+        let heap = Heap.init(by: <);
       
         // Insert array
         for val in nums {
            heap.add(val)
         }
-        
+
         // Get kth num
-        for _ in 1...k {
+        for _ in 1..<k {
           _ = heap.extract()
         }
         return heap.extract()
@@ -25,25 +25,90 @@ class Solution {
 }
 
 class Heap {
-    var values = [Int]();
+    var values = [0];
     
-    public func  add(_ val: Int){
-    //    values.append
+//    let comperator = {(_ n1: Int, _ n2: Int) -> Bool in n1 < n2} // smallest
+    var comperator: (Int, Int) -> Bool;
+    
+    init(by: @escaping (_ n1: Int, _ n2: Int) -> Bool){
+        self.comperator = by;
+    }
+    
+    public func add(_ val: Int){
         values.append(val)
         self.heapifyUp()
     }
 
     public func extract() -> Int? {
-        let top = values[0]
-        if let popped = self.values.popLast() {
-            values[0] = popped
-            self.heapifyDown()
-        }
-        return top
+        guard self.hasValues() else { return nil }
+        if self.hasOnlyOne() { return values.popLast() }
+        let first = self.popFirstAndReplaceWithLast()
+        self.heapifyDown()
+        return first
     }
     
-    private func heapifyUp(){}
-    private func heapifyDown(){}
+    private func heapifyUp(){
+        var idx = values.count - 1 // start from idx of last item
+        while(
+            self.hasParent(idx) &&
+            self.comperator(self.values[idx], self.getParent(idx)!)
+        ){
+            let parentIdx = self.getParentIdx(idx)
+            self.values.swapAt(idx, parentIdx)
+            idx = parentIdx
+        }
+    }
     
+    private func heapifyDown(){
+        var idx = 1; // top idx
+        while(self.hasLeftChild(idx)){
+            let shouldChooseRight = self.hasRightChild(idx)
+                && self.comperator(
+                    self.getRightChild(idx)!,
+                    self.getLeftChild(idx)!
+                )
+            
+            let childIdx = shouldChooseRight ? self.getRightChildIdx(idx) : self.getLeftChildIdx(idx)
+            self.values.swapAt(idx, childIdx)
+            idx = childIdx
+            continue;
+        }
+    }
+
+    //
+    private func hasValues () -> Bool { return values.count > 1 }
+    private func hasOnlyOne () -> Bool { return values.count == 2 }
+    
+    // move
+    private func popFirstAndReplaceWithLast() -> Int {
+        let first = self.values[1]
+        let last = self.values.popLast()!
+        self.values[1] = last
+        return first
+    }
+    
+    // has
+    private func hasParent(_ idx: Int) -> Bool { return idx != 1 }
+    private func hasLeftChild (_ idx: Int) -> Bool { return self.getLeftChildIdx(idx) < self.values.count }
+    private func hasRightChild(_ idx: Int) -> Bool { return self.getRightChildIdx(idx) < self.values.count }
+    
+    // idx getter
+    private func getParentIdx(_ idx: Int) -> Int { return idx / 2 }
+    private func getLeftChildIdx (_ idx: Int) -> Int { return 2 * idx }
+    private func getRightChildIdx(_ idx: Int) -> Int { return (2 * idx) + 1 }
+    
+    // val getter
+    private func getParent(_ idx: Int) -> Int? {
+        guard self.hasParent(idx) else { return nil }
+        return self.values[self.getParentIdx(idx)]
+    }
+    private func getLeftChild(_ idx: Int) -> Int? {
+        guard self.hasLeftChild(idx) else { return nil }
+        return self.values[self.getLeftChildIdx(idx)]
+    }
+    private func getRightChild(_ idx: Int) -> Int? {
+        guard self.hasRightChild(idx) else { return nil }
+        return self.values[self.getRightChildIdx(idx)]
+    }
 }
 
--- heap/main.swift
+++ heap/main.swift
@@ -7,9 +7,30 @@
 
 import Foundation
 
-print("Hello, World!")
+let testCaseList = [
+    [9,8,7,6,5,4,3,2,1],
+    [1,2,3,4,5,6,7,8,9],
+    [0,3,5,7,1,4,5,6],
+    [2,2,3,3,4,5,2,2,3],
+    [-1,-2,3,4,-100,100],
+    [-1,-2,-3,-4,-5,-6,-7,-8]
+]
 
+let pickedNum = 3
 
-if let result = Solution.findKthLargest([9,8,7,6,5,4,3,2,1], 3) {
-    print("> ", result)
+func doTest(){
+    for testCase in testCaseList {
+        let expected = testCase.sorted(by: <)[pickedNum - 1]
+        let result = Solution.findKthLargest(testCase, pickedNum)!
+        let resultStr = expected == result ? "  OK" : "x NG"
+        print(resultStr, "|",
+              "expeted:", expected,
+              "result:", result,
+              "testCase:",testCase
+              
+        );
+    }
+
 }
+
+doTest()
